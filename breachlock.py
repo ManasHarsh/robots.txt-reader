5d287bd3b8aed14dad54224f45c2cd18cc1bdb5f
Manas Harsh <33492814+ManasHarsh@users.noreply.github.com>
2024-02-19 14:58:26 +0530

    Add files via upload

---
+++ breachlock.py
@@ -0,0 +1,20 @@
+import requests
+
+def fetch_robots_txt(url):
+    try:    
+        robots_url = url.rstrip("/") + "/robots.txt"
+                
+        response = requests.get(robots_url)
+        
+        if response.status_code == 200:
+            return response.text
+        else:
+            return f"Failed to fetch robots.txt. Status code: {response.status_code}"
+    except Exception as e:
+        return f"An error occurred: {str(e)}"
+
+if __name__ == "__main__":
+    website_url = input("Enter the website URL: ")
+    robots_content = fetch_robots_txt(website_url)
+    print("\nContents of robots.txt file:")
+    print(robots_content)
